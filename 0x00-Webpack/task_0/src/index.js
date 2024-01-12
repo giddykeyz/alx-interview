86ce7da666fcb2275fbc278fed09ad2facf72132
giddykeyz <bardigideon@gmail.com>
2024-01-12 14:15:25 +0100

    webpack task

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+
+// Add three different paragraphs to the page body
+$(document).ready(function () {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+});
+
